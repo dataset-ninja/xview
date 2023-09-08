2ecb36a0296f9bd0c24ffe864adf7d86c2822f41
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-09-08 15:49:56 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "xView 2018"
 PROJECT_NAME_FULL: str = "DIUx xView: Objects in Context in Overhead Imagery 2018 Challenge"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
